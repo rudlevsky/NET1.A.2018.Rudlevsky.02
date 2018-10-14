97390aa0a35e42df3d06d23e67e2886c029c4628
rudlevsky <rudlevsky@gmail.com>
2018-10-14 13:33:40 +0300

    Final version (tests added)

---
+++ BitInserter/BitInserter.Tests/InsertNumberNTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BitInserter.Tests
+{
+    [TestFixture]
+    public class InsertNumberNTests
+    {
+        [TestCase(8, 15, 3, 8,ExpectedResult = 120)]
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        public int InsertNumberMethod_CorrectData_ReturnNewIntegerNumber(int firstNum, int secondNum, int firstInd, int secondInd)
+        {
+            return Inserter.InsertNumber(firstNum, secondNum, firstInd, secondInd);
+        }
+
+        [Test]
+        public void InsertNumberMethod_FirstIndBiggerThanSecondInd_ArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Inserter.InsertNumber(8, 15, 10, 5));
+        }
+
+        [Test]
+        public void InsertNumberMethod_IndexArgsLowerThanZero_ArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Inserter.InsertNumber(8, 15, -1, -1));
+        }
+
+        [Test]
+        public void InsertNumberMethod_IndexArgsBiggerThanRange_ArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Inserter.InsertNumber(8, 15, 32, 32));
+        }
+    }
+}
---
+++ BitInserter/BitInserter.Tests/InsertNumberTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitInserter.Tests
+{
+    [TestClass]
+    public class InsertNumberTests
+    {
+        [TestMethod]
+        public void InsertNumberMethod_CorrectData_ReturnNewIntegerNumber()
+        {
+            int expectedNumber = 120;
+
+            int actNumber = Inserter.InsertNumber(8, 15, 3, 8);
+
+            Assert.AreEqual(expectedNumber, actNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InsertNumberMethod_FirstIndBiggerThanSecondInd_ArgumentOutOfRangeException()
+        {
+            Inserter.InsertNumber(8,15,10,5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InsertNumberMethod_IndexArgsLowerThanZero_ArgumentOutOfRangeException()
+        {
+            Inserter.InsertNumber(8, 15, -1, -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InsertNumberMethod_IndexArgsBiggerThanRange_ArgumentOutOfRangeException()
+        {
+            Inserter.InsertNumber(8, 15, 32, 32);
+        }
+    }
+}
